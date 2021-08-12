86743339d6f6048a9ceedf0afdc63f15a6e1b7f6
Andres12V <feli_610@hotmail.com>
2021-08-12 12:06:53 -0500

    Update 14 August

--- src/control/src/controller.py
+++ src/control/src/controller.py
@@ -6,6 +6,8 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Float64
 from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import JointState
+from rosgraph_msgs.msg import Clock
 from cv_bridge import CvBridge, CvBridgeError
 from time import sleep
 import cv2 as cv
@@ -50,21 +52,22 @@ def rotationMatrixToEulerAngles(R):
 class Controller:
     def __init__(self):
         rospy.init_node('control')
-        # Camera Image topic
+        self.sub_time = rospy.Subscriber('/clock', Clock, self.time_callback)
+        "Camera Image topic"
         self.sub_image = rospy.Subscriber('/camera/image', Image, self.cam_callback)
         self.bridge = CvBridge()
-        # POSE of the robot
+        "POSE of the robot"
         self.sub = rospy.Subscriber('/odom', Odometry, self.control_callback)
+        self.sub = rospy.Subscriber('/joint_states', JointState, self.pos_callback)
         #self.pub = rospy.Publisher('/my_turtlebot_control/cmd_vel', Twist, queue_size=10)
-        # Wheel velocity topics
+        "Wheel velocity topics"
         self.left_wheel_vel = rospy.Publisher('/wheel_left_velocity_controller/command', Float64, queue_size=10)
         self.right_wheel_vel = rospy.Publisher('/wheel_right_velocity_controller/command', Float64, queue_size=10)
         msg1 = Odometry()
         self.l_w_msg = Float64()
         self.r_w_msg = Float64()
         # self.msg = Twist()
-
-        # Infrared sensors
+        "Sonar sensors"
         # self.sub_laser1 = rospy.Subscriber('/my_project/sonar1', LaserScan, self.ls1_callback)
         # self.sub_laser2 = rospy.Subscriber('/my_project/sonar2', LaserScan, self.ls2_callback)
         # self.sub_laser3 = rospy.Subscriber('/my_project/sonar3', LaserScan, self.ls3_callback)
@@ -74,38 +77,39 @@ class Controller:
         self.aruco_msg = Pose()
 
         # Frecuency of the loop:
-        self.rate = rospy.Rate(100)
+        self.rate = rospy.Rate(50)
+    def time_callback(self, msg_time):
+        time = float(msg_time.clock.secs)+msg_time.clock.nsecs/1000000000
+        #print(time)
 
     def control_callback(self, msg1):
         global Button
         global theta
         global i
+        global j
         global int_e
+        global int_e_h
         global smallest
+        global Omega
+        global t
 
-        # Get the POSE of the robot:
+        "Get the POSE of the robot:"
         pos_x = round(msg1.pose.pose.position.x, 2)
         pos_y = round(msg1.pose.pose.position.y, 2)
         rot_q = msg1.pose.pose.orientation
 
-        # Get the euler angles:
+        "Get the euler angles:"
         (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
         theta_deg = round(theta*(180/np.pi),2)
-        print('Pose: x,y,theta', pos_x, pos_y, theta_deg)
 
-        # Flags that switch the go-to-goal behavior to obstacle avoidance behavior:
+        "Flags that switch the go-to-goal behavior to obstacle avoidance behavior:"
         Flag_r = 1
-        my_list.append(pos_y)
-
-        # Append the position of the robot in each iteration for the plot
-        cord_x.append(pos_x)
-        cord_y.append(pos_y)
 
         if Button==0:
             try:
                 # xd = 2.4
                 # yd = 0.0
-                # If the marker is detected reach the marker
+                "If the marker is detected reach the marker:"
                 if state=='M_detected':
                     if id_to_find ==12:
                         xd = 2.4
@@ -134,10 +138,16 @@ class Controller:
                     if id_to_find ==25:
                         xd = -2.8
                         yd = -8.0
-                # If there is no marker detected follow the designed path
+                "If there is no marker detected follow the designed path:"
                 if state=='No_M_detected':
-                    xd = round(my_listx[i], 2)
-                    yd = round(my_listy[i], 2)
+                    # xd = 0.5
+                    # yd = 0.5
+                    if i<29:
+                        xd = round(my_listx[i], 2)
+                        yd = round(my_listy[i], 2)
+                    else:
+                        xd = round(g_b_x[j], 2)
+                        yd = round(g_b_y[j], 2)
             except:
                 pass
             # xd = round(self.aruco_msg.position.x, 2)
@@ -146,79 +156,118 @@ class Controller:
             # yd = 2.0
             # xd = round(my_listx[i], 2)
             # yd = round(my_listy[i], 2)
-            #print('Desired Pose xd, yd', xd, yd)
+            print('Desired Pose xd, yd', xd, yd)
         elif Button==1:
             xd = 1.0
             yd = 6.0
             print('Desired Pose xd, yd', xd, yd)
-        print('--------',state)
+
+        #print('--------',state)
+        "Erros in the x & y coordinates:"
         ex = xd-pos_x
         ey = yd-pos_y
-        for it_x in my_listx:
-            for it_y in my_listy:
-                e_x = it_x-pos_x
-                e_y = it_y-pos_y
-                e_d = np.sqrt( (e_x*e_x) + (e_y*e_y) )
-                if smallest is None:
-                    smallest=e_d
-                elif e_x<smallest:
-                    smallest=e_d 
+        "Follow the closer point of the designed path:"
+        # for it_x in my_listx:
+        #     for it_y in my_listy:
+        #         e_x = it_x-pos_x
+        #         e_y = it_y-pos_y
+        #         e_d = np.sqrt( (e_x*e_x) + (e_y*e_y) )
+        #         if smallest is None:
+        #             smallest=e_d
+        #         elif e_x<smallest:
+        #             smallest=e_d
+        #         if smallest==e_d:
+        #             #i = index(e_x)
+        #             af= 1
 
 
-        # Calculation of the total error in X and Y positions
+        "Calculation of the total error in X and Y positions:"
         e_t=np.abs(ex)+np.abs(ey)
-
         if state=='M_detected':
             if e_t==0:
                 theta_d=theta
             else:
                 theta_d = np.arctan2(ey, ex)
         if state=='No_M_detected':
-            if e_t==0:
+            if e_t<=0.15:
                 theta_d=theta
             else:
                 theta_d = np.arctan2(ey, ex)
-
+        "Orientation error:"
         e_heading = (theta_d - theta)
-        print('Desired Posew xd, yd', xd, yd, theta_d)
 
-        # Control signals:
+        "Control signals:"
         e_diag = np.sqrt( (ex*ex) + (ey*ey) )
         int_e+=e_diag
-        Vel = ( 0.1*e_diag )*Flag_r
-        Omega = ( 2*np.arctan2( np.sin(e_heading), np.cos(e_heading) ) )*Flag_r
-
-        # Get the required velocity of each wheel to reach the goal
-        self.l_w_msg = (1/(2*R))*(2*Vel-Omega*L)
-        self.r_w_msg = (1/(2*R))*(2*Vel+Omega*L)
-
-        # If ArUco marker is detected, reach the marker and wait for the confirmation
+        derivate_e.append(e_diag)
+        try:
+            d_e = (derivate_e[-1]-derivate_e[-2]) /0.01
+        except:
+            d_e = 0
+        int_e_h+=e_heading
         if state=='M_detected':
-            # If the button have not been pressed and the total error is 0, the goal
-            # have been reached, so the program waits for the confirmation button
+            Vel = ( 0.1*e_diag )*Flag_r
+            Omega = ( 1*np.arctan2( np.sin(e_heading), np.cos(e_heading) ) )*Flag_r
+        if state=='No_M_detected':
+            Vel = ( 0.08*e_diag+0.0001*int_e)*Flag_r
+            Omega = ( 0.6*np.arctan2( np.sin(e_heading), np.cos(e_heading)) )*Flag_r
+
+        "Get the required velocity of each wheel to reach the goal"
+        self.l_w_msg = (1/(2*R))*(2*Vel-Omega*L)
+        #self.l_w_msg = 5
+        if self.l_w_msg >= 10:
+             self.l_w_msg = 10
+        if self.l_w_msg <= -10:
+             self.l_w_msg = -10
+        self.r_w_msg = (1/(2*R))*(2*Vel+Omega*L)
+        if self.r_w_msg >= 10:
+             self.r_w_msg = 10
+        if self.r_w_msg <= -10:
+             self.r_w_msg = -10
+        "If ArUco marker is detected, reach the marker and wait for the confirmation"
+        if state=='M_detected':
+            """ If the button have not been pressed and the total error is 0, then the goal
+            have been reached, so the program waits for the confirmation button"""
             if Button==0:
-                if e_t==0.08:
+                if e_t==0.0:
                     Button = input('Enter 1: ')
                     my_list.clear()
             elif Button==1:
-            # If the button have been pressed and the total error is 0, the robot
-            # have been arrived to the initial position, then waits for a new table
-            # request
+            # """If the button have been pressed and the total error is 0, the robot
+            # have been arrived to the initial position, then waits for a new table request"""
                 if e_t==0.0:
                     Button = 0
                     aruco_num = input('Enter ArUco Marker id: ')
-        # If no ArUco marker is detected, set the designed path points as the goal
+
+        "If no ArUco marker is detected, set the designed path points as the goal"
         if state=='No_M_detected':
-            if e_t<=0.2:
-                if i<59:
+            if e_t<=0.15:
+                int_e=0
+                if i<29:
                     i+=1
                 else:
-                    i=59
-        # Publish the messages to the topics
+                    i=29
+                    if j<14:
+                        j+=1
+                    else:
+                        j=14
+
+        "Publish the messages to the topics:"
         self.left_wheel_vel.publish(self.l_w_msg)
         self.right_wheel_vel.publish(self.r_w_msg)
+        t += 0.02
+        print('My_Pose', pos_x, pos_y, theta_deg)
+        "Append the position of the robot in each iteration for the plot"
+        cord_x.append(pos_x)
+        cord_y.append(pos_y)
+        vec_th.append(theta)
+        ref_x.append(xd)
+        ref_y.append(yd)
+        ref_th.append(theta_d)
+        mein_w1.append(self.l_w_msg)
+        mein_w2.append(self.r_w_msg)
+        my_t.append(t)
         self.rate.sleep()
-
 
     def cam_callback(self, data):
         global state
@@ -293,7 +342,7 @@ class Controller:
             # else:
             #     state = 'No_M_detected'
             # Display the frame
-            cv.imshow('frame', aruco_image)
+            # cv.imshow('frame', aruco_image)
             #cv.imwrite('/home/andresvergara/images_aruco/pics/img4.jpg', cv_image)
             try:
                 pos_ar_x = 0.01*tvec[0]
@@ -389,11 +438,26 @@ class Controller:
         self.left_wheel_vel.publish(self.l_w_msg)
         self.right_wheel_vel.publish(self.r_w_msg)
 
-
+    def pos_callback(self, msg_pos):
+        pos_w1 = msg_pos.position[0]
+        pos_w2 = msg_pos.position[1]
+        x_e=1
+        y_e=6
+        th_e=-1.5708
+        #print('Pos wheels', pos_w1, pos_w2)
+        Dc = ((pos_w1+pos_w2)*0.5)*0.0325
+        # print('Dc', Dc)
+        p_x = x_e+Dc*np.cos(theta)
+        p_y = y_e+Dc*np.sin(theta)
+        t_r = th_e+0.0325*(pos_w1-pos_w2)/L
+        t_rd = round(t_r*(180/np.pi),2)
+        # print('Mein pose', p_x, p_y, t_r, t_rd)
+        posx_enc.append(p_x)
+        posy_enc.append(p_y)
 
 
 if __name__ == '__main__':
-    # Number of the table request:
+    "Number of the table request:"
     aruco_num = input('Enter ArUco Marker id: ')
     Flag1 = 0
     Flag2 = 0
@@ -401,56 +465,107 @@ if __name__ == '__main__':
     Flag4 = 0
     Button = 0
     int_e = 0
+    int_e_h = 0
+    Omega = 0
     state = 'No_M_detected'
     i = 0
-    R = 0.065
+    j = 0
+    R = 0.0325
     L = 0.3
     global smallest
     smallest = None
+    t=0
 
     my_list = list()
     cord_x=list()
     cord_y=list()
 
+    mein_w1=list()
+    mein_w2=list()
+
+    derivate_e = list()
+
+    my_t = list()
+    ref_x = list()
+    ref_y = list()
+    ref_th = list()
+    vec_th = list()
+    posx_enc = list()
+    posy_enc = list()
+
     # Path to follow when no ArUco marker is detected:
     my_listx = list()
     my_listy = list()
-    tray_x=np.linspace(1.0, 1.1, num=10)
-    tray_y=np.linspace(6.0, 6.0, num=10)
-    tray_x2=np.linspace(1.1, 1.1, num=10)
-    tray_y2=np.linspace(6.0, -7.25, num=10)
-    tray_x3=np.linspace(1.1, 1.0, num=10)
-    tray_y3=np.linspace(-7.25, -7.25, num=10)
-    tray_x4=np.linspace(1.0, 1.0, num=10)
-    tray_y4=np.linspace(-7.25, 0, num=10)
-    tray_x5=np.linspace(1.0, -1.5, num=10)
-    tray_y5=np.linspace(0, 0, num=10)
-    tray_x6=np.linspace(-1.5, -1.5, num=10)
-    tray_y6=np.linspace(0, -7.25, num=10)
-    for n in range(0,10):
+    # tray_x=np.linspace(0, 0, num=2)
+    # tray_y=np.linspace(0.4, 0.8, num=2)
+    # tray_x2=np.linspace(0.4, 0.6, num=2)
+    # tray_y2=np.linspace(0.8, 1.1, num=2)
+    # tray_x3=np.linspace(0.93, 0.93, num=2)
+    # tray_y3=np.linspace(1.1, 1.35, num=2)
+    # tray_x4=np.linspace(0.93, 0.4, num=2)
+    # tray_y4=np.linspace(1.35, 1.35, num=2)
+    # tray_x5=np.linspace(0, 0, num=2)
+    # tray_y5=np.linspace(1.35, 0.9, num=2)
+    # tray_x6=np.linspace(0, 0, num=2)
+    # tray_y6=np.linspace(0, 0, num=2)
+    tray_x=np.linspace(1.0, 1.1, num=5)
+    tray_y=np.linspace(6.0, 6.0, num=5)
+    tray_x2=np.linspace(1.1, 1.1, num=5)
+    tray_y2=np.linspace(6.0, -7.25, num=5)
+    tray_x3=np.linspace(1.1, 1.0, num=5)
+    tray_y3=np.linspace(-7.25, -7.25, num=5)
+    tray_x4=np.linspace(1.0, 1.0, num=5)
+    tray_y4=np.linspace(-7.25, 0, num=5)
+    tray_x5=np.linspace(1.0, -1.5, num=5)
+    tray_y5=np.linspace(0, 0, num=5)
+    tray_x6=np.linspace(-1.5, -1.5, num=5)
+    tray_y6=np.linspace(0, -7.25, num=5)
+    for n in range(0,5):
         my_listx.append(tray_x[n])
-    for n1 in range(0,10):
+    for n1 in range(0,5):
         my_listx.append(tray_x2[n1])
-    for n2 in range(0,10):
+    for n2 in range(0,5):
         my_listx.append(tray_x3[n2])
-    for n3 in range(0,10):
+    for n3 in range(0,5):
         my_listx.append(tray_x4[n3])
-    for n4 in range(0,10):
+    for n4 in range(0,5):
         my_listx.append(tray_x5[n4])
-    for n5 in range(0,10):
+    for n5 in range(0,5):
         my_listx.append(tray_x6[n5])
-    for m in range(0,10):
+    for m in range(0,5):
         my_listy.append(tray_y[m])
-    for m1 in range(0,10):
+    for m1 in range(0,5):
         my_listy.append(tray_y2[m1])
-    for m2 in range(0,10):
+    for m2 in range(0,5):
         my_listy.append(tray_y3[m2])
-    for m3 in range(0,10):
+    for m3 in range(0,5):
         my_listy.append(tray_y4[m3])
-    for m4 in range(0,10):
+    for m4 in range(0,5):
         my_listy.append(tray_y5[m4])
-    for m5 in range(0,10):
+    for m5 in range(0,5):
         my_listy.append(tray_y6[m5])
+    ## Path to go back to the initial point
+    g_b_x = list()
+    g_b_y = list()
+    go_back_x=np.linspace(-1.5, -1.5, num=5)
+    go_back_y=np.linspace(-7.25, 0, num=5)
+    go_back_x2=np.linspace(-1.5, 1.0, num=5)
+    go_back_y2=np.linspace(0, 0, num=5)
+    go_back_x3=np.linspace(1.0, 1.0, num=5)
+    go_back_y3=np.linspace(0, 6.0, num=5)
+    for n6 in range(0,5):
+        g_b_x.append(go_back_x[n6])
+    for n7 in range(0,5):
+        g_b_x.append(go_back_x2[n7])
+    for n8 in range(0,5):
+        g_b_x.append(go_back_x3[n8])
+
+    for m6 in range(0,5):
+        g_b_y.append(go_back_y[m6])
+    for m7 in range(0,5):
+        g_b_y.append(go_back_y2[m7])
+    for m8 in range(0,5):
+        g_b_y.append(go_back_y3[m8])
 
     ## This part is for the real camera ##
     # cap=cv.VideoCapture(0)
@@ -462,18 +577,36 @@ if __name__ == '__main__':
         if not rospy.is_shutdown():
             rospy.spin()
             plt.plot(cord_x,cord_y, 'g')
-            plt.plot(1,6, 'x')
-            plt.plot(2.4,-2, 'o')
-            plt.plot(2.4,-5.5, 'o')
-            plt.plot(2.4,-9, 'o')
-            plt.plot(-.2,-2, 'o')
-            plt.plot(-.2,-5.5, 'o')
-            plt.plot(-.2,-9, 'o')
-            plt.plot(-2.8,-2, 'o')
-            plt.plot(-2.8,-5.5, 'o')
-            plt.plot(-2.8,-9, 'o')
-            plt.plot(tray_x6[9],tray_y6[9], 'D')
-            plt.plot(my_listx,my_listy, 'b')
+            plt.plot(1,6, 'x',  label='Starting point')
+            plt.plot(2.4,-2, 'ro', label='Tables')
+            plt.plot(2.4,-5.5, 'ro')
+            plt.plot(2.4,-9, 'ro')
+            plt.plot(-.2,-2, 'ro')
+            plt.plot(-.2,-5.5, 'ro')
+            plt.plot(-.2,-9, 'ro')
+            plt.plot(-2.8,-2, 'ro')
+            plt.plot(-2.8,-5.5, 'ro')
+            plt.plot(-2.8,-9, 'ro')
+            plt.plot(tray_x6[4],tray_y6[4], 'kD', label='Final point')
+            plt.figure(1)
+            plt.plot(my_listx,my_listy, 'b--', label='Trajectory')
+            plt.legend()
+            plt.figure(2)
+            plt.plot(mein_w1, 'g', label='Left_wheel velocity')
+            plt.plot(mein_w2, 'r', label='Right wheel velocity')
+            plt.legend()
+            plt.figure(3)
+            plt.plot(my_t, ref_x, 'r--', label='Ref x')
+            plt.plot(my_t, cord_x, 'g', label='Pos x')
+            plt.plot(my_t, ref_y, 'k--', label='Ref y')
+            plt.plot(my_t, cord_y, 'b', label='Pos y')
+            plt.legend()
+            # plt.plot(my_t, posx_enc, 'mo')
+            # plt.plot(my_t, posy_enc, 'yo')
+            plt.figure(4)
+            plt.plot(my_t, ref_th, 'r--', label='Theta desired')
+            plt.plot(my_t, vec_th, 'g', label='Theta')
+            plt.legend()
             plt.show()
 
     except rospy.ROSInterruptException as e:
